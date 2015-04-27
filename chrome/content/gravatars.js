7ab23d17e4235dbfffb858d479a72ac01c3a5f33
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2015-04-27 20:46:03 +0200

    add missing semicolons

--- chrome/content/gravatars.js
+++ chrome/content/gravatars.js
@@ -25,17 +25,17 @@ gravatars.columnHandler = {
   getRowProperties: function(row, props) { },
 
   getSortLongForRow: function(hdr) { return 0; },
-}
+};
 
 
 gravatars.getAuthorFromHdr = function(hdr) {
   let author = hdr.mime2DecodedAuthor
   return gravatars.firstEmailFromHeader(author);
-}
+};
 
 gravatars.getAuthorAtRow = function(row) {
   return gravatars.getAuthorFromHdr(gDBView.getMsgHdrAt(row));
-}
+};
 
 gravatars.firstEmailFromHeader = function(header, query) {
   const gHeaderParser = Cc["@mozilla.org/messenger/headerparser;1"].getService(Ci.nsIMsgHeaderParser);
@@ -47,7 +47,7 @@ gravatars.firstEmailFromHeader = function(header, query) {
   let numberOfParsedAddresses = gHeaderParser.parseHeadersWithArray(header, mailboxes, names, fullNames);
 
   return { name: names.value[0], fullName: fullNames.value[0], mailbox: mailboxes.value[0] };
-}
+};
 
 gravatars.addressBookPicture = function(email) {
   return null; // NOT IMPLEMENTED YET
@@ -59,32 +59,32 @@ gravatars.addressBookPicture = function(email) {
     return null;
   }
   // FIXME return a photo if there is one
-}
+};
 
 gravatars.photo = function(email) {
   return gravatars.addressBookPicture(email) || gravatars.gravatar(email);
-}
+};
 
 gravatars.gravatar = function(email) {
   let hash = GlodaUtils.md5HashString(email.toLowerCase().trim());
   photoURI = "http://www.gravatar.com/avatar/" + encodeURIComponent(hash) + '?s=16&d=identicon';
   return photoURI;
-}
+};
 
 gravatars.init = function() {
   var ObserverService = Cc["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
   ObserverService.addObserver(gravatars.CreateDbObserver, "MsgCreateDBView", false);
-}
+};
 
 gravatars.CreateDbObserver = {
   observe: function(aMsgFolder, aTopic, aData) {
      gravatars.addCustomColumnHandler();
   }
-}
+};
 
 gravatars.addCustomColumnHandler = function() {
    gDBView.addColumnHandler("colGravatar", gravatars.columnHandler);
-}
+};
 
 
 window.addEventListener("load", function initGravatars(event) {

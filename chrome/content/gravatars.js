6807009db4ca5b85560e58f3ac3d7ae45dc4d569
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2015-04-27 20:27:32 +0200

    introduce namespacing, separate init function

--- chrome/content/gravatars.js
+++ chrome/content/gravatars.js
@@ -1,3 +1,8 @@
+// a namespace for this add-on
+if (typeof gravatars == "undefined") {
+  var gravatars = {};
+};
+
 if (!defaultPhotoURI) {
   var defaultPhotoURI = "chrome://messenger/skin/addressbook/icons/contact-generic.png";
 }
@@ -6,18 +11,18 @@ const MAILBOX = 0
 const NAME = 1
 const FULL_NAME = 2
 
-var columnHandler = {
+gravatars.columnHandler = {
   getCellText: function(row, col) {
     // TODO show the name from the address book if possible
     // TODO it is doing the work twice, rewrite to parseEmailHeader just once
-    return this.getAuthorName(row) || this.getAuthorMailbox(row);
+    return gravatars.getAuthorName(row) || gravatars.getAuthorMailbox(row);
   },
 
   getImageSrc: function(row, col) {
-    return this.photo(this.getAuthorMailbox(row));
+    return gravatars.photo(gravatars.getAuthorMailbox(row));
   },
 
-  getSortStringForRow: function(hdr) { return this.parseEmailsFromHeader(hdr.mime2DecodedAuthor, 'mailbox')[0]; },
+  getSortStringForRow: function(hdr) { return gravatars.parseEmailsFromHeader(hdr.mime2DecodedAuthor, 'mailbox')[0]; },
 
   isString: function() { return true; },
 
@@ -26,75 +31,80 @@ var columnHandler = {
   getRowProperties: function(row, props) { },
 
   getSortLongForRow: function(hdr) { return 0; },
+}
 
+gravatars.getAuthor = function(row) {
+  return gDBView.getMsgHdrAt(row).mime2DecodedAuthor;
+}
 
-  // --- helper methods ---
+gravatars.getAuthorName = function(row) {
+  return gravatars.parseEmailsFromHeader(gravatars.getAuthor(row), NAME)[0]
+}
 
-  getAuthor: function(row) { return gDBView.getMsgHdrAt(row).mime2DecodedAuthor; },
-
-  getAuthorName: function(row) { return this.parseEmailsFromHeader(this.getAuthor(row), NAME)[0] },
-
-  getAuthorMailbox: function(row) { return this.parseEmailsFromHeader(this.getAuthor(row), MAILBOX)[0] },
+gravatars.getAuthorMailbox = function(row) {
+  return gravatars.parseEmailsFromHeader(gravatars.getAuthor(row), MAILBOX)[0]
+}
 
   // header should be the value of to, from, or cc
-  parseEmailsFromHeader: function(header, query) {
-    const gHeaderParser = Cc["@mozilla.org/messenger/headerparser;1"].getService(Ci.nsIMsgHeaderParser);
+gravatars.parseEmailsFromHeader = function(header, query) {
+  const gHeaderParser = Cc["@mozilla.org/messenger/headerparser;1"].getService(Ci.nsIMsgHeaderParser);
 
-    let mailboxes = {};
-    let names = {};
-    let fullNames = {};
+  let mailboxes = {};
+  let names = {};
+  let fullNames = {};
 
-    let numberOfParsedAddresses = gHeaderParser.parseHeadersWithArray(header, mailboxes, names, fullNames);
+  let numberOfParsedAddresses = gHeaderParser.parseHeadersWithArray(header, mailboxes, names, fullNames);
 
-    if (query === NAME) {
-      return names.value;
-    } else if (query === FULL_NAME) {
-      return fullNames.value;
-    } else if (query == MAILBOX) {
-      return mailboxes.value;
-    }
-  },
-
-  addressBookPicture: function(email) {
-    return null; // NOT IMPLEMENTED YET
-
-    // .. FIXME figure out how to grab all address books
-    let card = collection.cardForEmailAddress(email);
-    if (card == null) {
-      return null;
-    }
-    // FIXME return a photo if there is one
-  },
-
-  photo: function(email) {
-    return this.addressBookPicture(email) || this.gravatar(email);
-  },
-
-  gravatar: function(email) {
-    let hash = GlodaUtils.md5HashString(email.toLowerCase().trim());
-    photoURI = "http://www.gravatar.com/avatar/" + encodeURIComponent(hash) + '?s=16&d=identicon';
-    return photoURI;
+  if (query === NAME) {
+    return names.value;
+  } else if (query === FULL_NAME) {
+    return fullNames.value;
+  } else if (query == MAILBOX) {
+    return mailboxes.value;
   }
 }
 
+gravatars.addressBookPicture = function(email) {
+  return null; // NOT IMPLEMENTED YET
+
+  // .. FIXME figure out how to grab all address books
+  let card = collection.cardForEmailAddress(email);
+  if (card == null) {
+    return null;
+  }
+  // FIXME return a photo if there is one
+}
+
+gravatars.photo = function(email) {
+  return gravatars.addressBookPicture(email) || gravatars.gravatar(email);
+}
+
+gravatars.gravatar = function(email) {
+  let hash = GlodaUtils.md5HashString(email.toLowerCase().trim());
+  photoURI = "http://www.gravatar.com/avatar/" + encodeURIComponent(hash) + '?s=16&d=identicon';
+  return photoURI;
+}
 
 
-
-window.addEventListener("load", loadGravatars, false);
-
-function loadGravatars() {
-  window.removeEventListener("load", loadGravatars, false); // remove listener, no longer needed
-
+gravatars.init = function() {
   var ObserverService = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
-  ObserverService.addObserver(CreateDbObserver, "MsgCreateDBView", false);
+  ObserverService.addObserver(gravatars.CreateDbObserver, "MsgCreateDBView", false);
 }
 
-var CreateDbObserver = {
+gravatars.CreateDbObserver = {
   observe: function(aMsgFolder, aTopic, aData) {
-     addCustomColumnHandler();
+     gravatars.addCustomColumnHandler();
   }
 }
 
-function addCustomColumnHandler() {
-   gDBView.addColumnHandler("colGravatar", columnHandler);
+gravatars.addCustomColumnHandler = function() {
+   gDBView.addColumnHandler("colGravatar", gravatars.columnHandler);
 }
+
+
+window.addEventListener("load", function initGravatars(event) {
+    // remove listener, no longer needed
+    window.removeEventListener("load", initGravatars, false);
+    // do the real initialization
+    gravatars.init()
+},false);
